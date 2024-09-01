3724085d528365ab2027de8984464ea357b01433
Niklas <57962465+niklasnieberler@users.noreply.github.com>
2024-09-01 13:52:38 +0200

    remove shadowJar in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    id("com.github.johnrengelman.shadow") version "8.1.1"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.serialization") version "1.9.0"
     `maven-publish`
@@ -17,10 +16,6 @@ repositories {
 dependencies {
     // avionik dependencies
     compileOnly("world.avionik:config-kit:1.0.2")
-}
-
-tasks.named("shadowJar", ShadowJar::class) {
-    mergeServiceFiles()
 }
 
 publishing {
